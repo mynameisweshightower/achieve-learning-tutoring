921a353b3e36f763cd9e2c9c153ca8c7c17dd2af
Wes Hightower <bundleofstrings@gmail.com>
2018-03-28 19:56:27 -0500

    Initial upload
    Add index file; CSS and includes folder.

---
+++ includes/head.php
@@ -0,0 +1,13 @@
+<head>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="theme-color" content="#005e6c">
+	<meta name="msapplication-navbutton-color" content="#005e6c">
+	<!-- iOS Safari -->
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-status-bar-style" content="#005e6c">
+	<link rel="shortcut icon" href="favicon.png" type="image/png" />
+
+	<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
---
+++ includes/header.php
@@ -0,0 +1,15 @@
+<header id="header">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<a href="/">
+					<img src="#" class="img-responsive" />
+				</a>
+			</div>
+			<div class="col-md-6">
+				<a href="tel:+15122644649"><i class="fas fa-phone" data-fa-transform="flip-h"></i>512-264-4649</a>
+			</div>
+		</div>
+	</div>
+	<?php include 'nav.php' ?>
+</header>
